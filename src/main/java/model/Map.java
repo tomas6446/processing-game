1969c1f945af9d41b3164aea9c596cc6d9a2cda1
tomas <tomas.kozakas@gmail.com>
2022-09-27 10:13:40 +0300

    update size of objects

--- src/main/java/model/Map.java
+++ src/main/java/model/Map.java
@@ -18,19 +18,19 @@ import java.util.List;
 @Setter
 public class Map {
     /* param of map objects */
-    public static final int SPEED = 3;
+    public static final int SPEED = 4;
     private static final int PLAYER_WIDTH = 36;
     private static final int PLAYER_HEIGHT = 60;
-    private static final int ENEMY_WIDTH = 48;
-    private static final int ENEMY_HEIGHT = 48;
-    private static final int FLOOR_WIDTH = 48;
-    private static final int FLOOR_HEIGHT = 48;
-    private static final int WALL_WIDTH = 48;
-    private static final int WALL_HEIGHT = 48;
+    private static final int ENEMY_WIDTH = 64;
+    private static final int ENEMY_HEIGHT = 64;
+    private static final int FLOOR_WIDTH = 64;
+    private static final int FLOOR_HEIGHT = 64;
+    private static final int WALL_WIDTH = 64;
+    private static final int WALL_HEIGHT = 64;
     private static final int OBSTACLE_WIDTH = 32;
     private static final int OBSTACLE_HEIGHT = 32;
-    private static final int EXIT_WIDTH = 48;
-    private static final int EXIT_HEIGHT = 48;
+    private static final int EXIT_WIDTH = 64;
+    private static final int EXIT_HEIGHT = 64;
     private static final int BAR_WIDTH = 64;
     private static final int BAR_HEIGHT = 64;
     private static final int BACKGROUND_WIDTH = 1920;
@@ -91,10 +91,8 @@ public class Map {
                         case 2 -> addObject(ObjectType.WALL, x, y, i, j);
                         case 4 -> addObject(ObjectType.EXIT, x, y, i, j);
                         case 5 -> addObject(ObjectType.PLAYER, x, y, i, j);
-                        case 3 -> {
-                            addObject(ObjectType.OBSTACLE, x, y, i, j);
-                            addObject(ObjectType.ENEMY, x, y, i, j);
-                        }
+                        case 3 -> addObject(ObjectType.ENEMY, x, y, i, j);
+
                         default -> throw new IllegalStateException("Unexpected value in the matrix: " + grid[i][j]);
                     }
                 }
@@ -109,22 +107,21 @@ public class Map {
             case WALL ->
                     staticObjects.set(index, new StaticObject(textures.get(2), x, y, WALL_WIDTH, WALL_HEIGHT, true, ObjectType.WALL)); /* wall */
             case TILE ->
-                    staticObjects.set(index, new StaticObject(textures.get(0), x, y, tileSize, tileSize, false, ObjectType.TILE));
+                    staticObjects.set(index, new StaticObject(textures.get(0), x, y, tileSize, tileSize, true, ObjectType.TILE));
             case HEALTH ->
                     healthBar = new HealthBar(textures.get(7), BAR_WIDTH, BAR_HEIGHT);
             case FLOOR ->
                     staticObjects.set(index, new StaticObject(textures.get(1), x, y, FLOOR_WIDTH, FLOOR_HEIGHT, false, ObjectType.FLOOR)); /* floor */
             case SKY ->
                     sky = new StaticObject(textures.get(8), -grid[0].length * tileSize / 2, 0, BACKGROUND_WIDTH, BACKGROUND_HEIGHT, false, ObjectType.SKY);
+            case EXIT ->
+                    staticObjects.set(index, new StaticObject(textures.get(4), x, y, EXIT_WIDTH, EXIT_HEIGHT, true, ObjectType.EXIT)); /* exit */
             case PLAYER -> {
                 staticObjects.set(index, new StaticObject(textures.get(1), x, y, FLOOR_WIDTH, FLOOR_HEIGHT, false, ObjectType.FLOOR)); /* floor */
                 player = new Player(textures.get(5), x + tileSize / 2 - PLAYER_WIDTH / 2, y + tileSize / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH, PLAYER_HEIGHT, 9, 4, SPEED); /* player */
             }
-            case EXIT -> {
-                staticObjects.set(index, new StaticObject(textures.get(1), x, y, FLOOR_WIDTH, FLOOR_HEIGHT, false, ObjectType.FLOOR)); /* floor */
-                staticObjects.set(index, new StaticObject(textures.get(4), x, y, EXIT_WIDTH, EXIT_HEIGHT, true, ObjectType.EXIT)); /* exit */
-            }
             case ENEMY -> {
+                staticObjects.set(index, new StaticObject(textures.get(1), x, y, FLOOR_WIDTH, FLOOR_HEIGHT, false, ObjectType.FLOOR)); /* floor */
                 enemies.add(new Enemy(textures.get(3), x, y, ENEMY_WIDTH, ENEMY_HEIGHT)); /* enemy */
                 waves.add(new Wave(textures.get(6), x + tileSize / 2 - OBSTACLE_WIDTH / 2, y + tileSize / 2 - OBSTACLE_HEIGHT / 2, OBSTACLE_WIDTH, OBSTACLE_HEIGHT)); /* enemy attacks */
             }
